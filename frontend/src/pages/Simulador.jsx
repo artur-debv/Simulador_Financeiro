3ac6e00a0d33543ecf838f4336923913be18e9ea
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2025-03-15 16:05:17 -0300

    Update Simulador.jsx

--- frontend/src/pages/Simulador.jsx
+++ frontend/src/pages/Simulador.jsx
@@ -10,7 +10,7 @@ function Simulador() {
     const init = parseFloat(investimento);
     const fluxoArray = fluxos.split(",").map((val) => parseFloat(val.trim()));
     try {
-      const response = await axios.post("http://127.0.0.1:5000/calcular", {
+      const response = await axios.post("https://simulador-financeiro-6naq.onrender.com/calcular", {
         investimento: init,
         fluxos: fluxoArray,
       });
